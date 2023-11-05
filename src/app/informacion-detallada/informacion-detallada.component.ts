93ce92442812551d4e0a85a75f4079436e7c60c6
AmandaCode1 <amandacode1337@gmail.com>
2023-11-05 15:14:51 +0100

    Buscador en el componente correcto

--- src/app/informacion-detallada/informacion-detallada.component.ts
+++ src/app/informacion-detallada/informacion-detallada.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-informacion-detallada',
   templateUrl: './informacion-detallada.component.html',
   styleUrls: ['./informacion-detallada.component.css']
 })
-export class InformacionDetalladaComponent {
+export class InformacionDetalladaComponent implements OnInit{
+
+  name: string = '';
+  urlImage: string = '';
+  tipo: string = '';
+
+  constructor(private pokemonService: PokemonService){}
+
+  ngOnInit(): void {
+  }
+
+  cargaPokemon(){
+    this.pokemonService.getPokemon(this.name).subscribe((data:any) => {
+      this.urlImage = data.sprites.other.dream_world.front_default,
+      this.tipo = data.types.map((tipo: any) => tipo.type.name);
+    })
+  }
 
 }
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -8,17 +8,6 @@ import { PokemonService } from '../pokemon.service';
 })
 export class ListaComponent implements OnInit{
 
-  name: string = '';
-  urlImage: string = '';
-
-  constructor(private pokemonService: PokemonService){}
-
   ngOnInit(): void {
-  }
-
-  cargaPokemon(){
-    this.pokemonService.getPokemon(this.name).subscribe((data:any) => {
-      this.urlImage = data.sprites.front_default
-    })
   }
 }
