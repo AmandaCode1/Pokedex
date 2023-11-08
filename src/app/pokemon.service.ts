b730f9c846114b3281b52870e4ffda33d3271830
benitogallardo <benito11gallardo@gmail.com>
2023-11-08 12:58:54 +0100

    lista

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -11,29 +11,7 @@ export class PokemonService {
   private url: string = 'https://pokeapi.co/api/v2/pokemon';
 
   constructor(private http: HttpClient) { }
-
-   /*getPokemons(count: number): Observable<Pokemon[]> {
-    
-   const requests: Observable<Pokemon>[] = [];
-    
-    for(let i = 1 ; i <= 150 ; i++){
-      this.http.get(`${this.url}/${i}`).pipe(map((data: any) => {
-        return {
-          id: data.id,
-          name: data.species.name,
-          image: data.sprites.other.dream_world.front_default,
-          types: data.types.map((tipo: any) => tipo.type.name),
-          peso: data.weight,
-          altura: data.height
-        }
-      }))
-
-      requests.push(request);
-    }
-    
-    return forkJoin(requests);
-  }*/
-
+  
   getPokemons(count: number): Observable<Pokemon[]> {
     const requests: Observable<Pokemon>[] = [];
   
