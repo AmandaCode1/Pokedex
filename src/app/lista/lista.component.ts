dbba7ba970fdfa0169aeae6cc9c2c095249f500a
AmandaCode1 <amandacode1337@gmail.com>
2023-11-13 11:38:27 +0100

    buscador  terminado

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -10,8 +10,10 @@ import { Pokemon } from '../model/pokemon';
 })
 export class ListaComponent implements OnInit{
 
-  pokemons: Pokemon [] = [];
+  pokemons: Pokemon [] = [];//guardamos la lista original
+  copiapokemons: Pokemon [] = [];//copia del primer array en el que haremos las busquedas
   nameP: string = '';
+  tipo: string = '';
 
   constructor(private pokemonService: PokemonService){}
 
@@ -22,13 +24,32 @@ export class ListaComponent implements OnInit{
    cargar(){
     this.pokemonService.getPokemons(493).subscribe((response:Pokemon[]) =>{
       this.pokemons = response;
+      this.copiapokemons = response;
     });
-    console.log(this.pokemons);
    }
 
    buscador(){
-    this.pokemons = this.pokemons.filter((pokemon) => pokemon.name == this.nameP);
-    console.log(this.pokemons);
+    this.cargarLista();
+    const pokemonBuscador = this.pokemons.filter((pokemon) => pokemon.name == this.nameP);
+    this.copiapokemons = pokemonBuscador;
+   }
+
+   generacion(inicio: number, fin: number){
+    this.cargarLista();
+    const pokemonGeneracion = this.pokemons.slice(inicio,fin);
+    this.copiapokemons = pokemonGeneracion;
+   }
+
+   tipos(tipo: string){
+    this.cargarLista();
+    const pokemonTipo = this.pokemons.filter((pokemon) => pokemon.types.includes(tipo));
+    this.copiapokemons = pokemonTipo;
+   }
+
+   cargarLista(){//Si la lista esta vacia por haber hecho una busqueda antes, volvemos a cargar los pokemon
+    if(this.pokemons.length === 0){
+      this.pokemons = [...this.copiapokemons];
+    }
    }
 
     
