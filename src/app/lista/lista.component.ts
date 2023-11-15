436463d40f5b09cee0ff22a11a256dfacf6e5661
AmandaCode1 <amandacode1337@gmail.com>
2023-11-15 11:08:10 +0100

    buscador con filtro

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -14,6 +14,8 @@ export class ListaComponent implements OnInit {
   copiapokemons: Pokemon[] = [];//copia del primer array en el que haremos las busquedas
   nameP: string = '';
   tipo: string = '';
+  inicio: number = 0;
+  fin: number = 0;
 
   constructor(private pokemonService: PokemonService) { }
 
@@ -24,33 +26,57 @@ export class ListaComponent implements OnInit {
   cargar() {
     this.pokemonService.getPokemons(493).subscribe((response: Pokemon[]) => {
       this.pokemons = response;
-      this.copiapokemons = response;
+      this.copiapokemons = [...response];
     });
   }
 
   buscador() {
-    this.cargarLista();
-    const pokemonBuscador = this.pokemons.filter((pokemon) => pokemon.name == this.nameP);
-    this.copiapokemons = pokemonBuscador;
+    if(!this.nameP){
+      this.cargar();
+    } else {
+      const pokemonBuscador = this.pokemons.filter((pokemon) => pokemon.name.toLowerCase().includes(this.nameP.toLowerCase()));
+      this.pokemons = pokemonBuscador;
+    }
   }
 
   generacion(inicio: number, fin: number) {
-    //this.cargarLista();
+    this.inicio = inicio;
+    this.fin = fin;
+    this.filtrar();
+  }
+
+  tipos(tipo: string) {
+    this.tipo = tipo;
+    this.filtrar();
+  }
+
+  /*generacion(inicio: number, fin: number) {
+    this.cargarLista();
     const pokemonGeneracion = this.pokemons.slice(inicio, fin);
     this.copiapokemons = pokemonGeneracion;
   }
 
   tipos(tipo: string) {
-    //this.cargarLista();
-    const pokemonTipo = this.pokemons.filter((pokemon) => pokemon.types.includes(tipo));
+    this.cargarLista();
+    const pokemonTipo = this.copiapokemons.filter((pokemon) => pokemon.types.includes(tipo));
     this.copiapokemons = pokemonTipo;
-  }
+  }*/
 
-  cargarLista() {//Si la lista esta vacia por haber hecho una busqueda antes, volvemos a cargar los pokemon
-    if (this.pokemons.length === 0) {
-      this.pokemons = [...this.copiapokemons];
+  filtrar(){
+    this.pokemons = [...this.copiapokemons];
+
+    if (this.inicio && this.fin) {
+      this.pokemons = this.pokemons.slice(this.inicio, this.fin);
     }
+  
+    if (this.tipo) {
+      this.pokemons = this.pokemons.filter(pokemon => pokemon.types.includes(this.tipo));
+    }
+
   }
 
-
+  listaPokemon(){
+      this.filtrar();
+      return this.pokemons;
+  }
 }
